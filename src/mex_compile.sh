b15779d189455b9682e5dcd794877760286e9a26
Ben Charrow <bcharrow@seas.upenn.edu>
2013-05-14 23:19:52 -0700

    Update mex_compile.sh for catkin build

--- src/mex_compile.sh
+++ src/mex_compile.sh
@@ -1,25 +1,10 @@
-mex -g rosbag_wrapper.cpp parser.cpp  \
-  ../lib/librosbag.a \
-  ../lib/librosconsole.a \
-  ../lib/libroscpp_serialization.a \
-  ../lib/libros.a \
-  ../lib/librostime.a \
-  ../lib/liblog4cxx.a \
-  ../lib/libaprutil-1.a \
-  ../lib/libapr-1.a \
-  ../lib/libexpat.a \
-  ../lib/libboost_filesystem.a \
-  ../lib/libboost_system.a \
-  ../lib/libboost_signals.a \
-  ../lib/libboost_thread.a \
-  ../lib/libboost_regex.a \
-  ../lib/libbz2.a \
--I/opt/ros/electric/stacks/ros_comm/tools/rosbag/include \
--I/opt/ros/electric/stacks/ros_comm/clients/cpp/roscpp/include \
--I/opt/ros/electric/stacks/ros_comm/clients/cpp/roscpp_serialization/include \
--I/opt/ros/electric/stacks/ros_comm/clients/cpp/roscpp_traits/include \
--I/opt/ros/electric/stacks/ros_comm/utilities/xmlrpcpp/src \
--I/opt/ros/electric/stacks/ros_comm/tools/rosconsole/include \
--I/opt/ros/electric/stacks/ros_comm/utilities/rostime/include \
--I/opt/ros/electric/stacks/ros_comm/utilities/cpp_common/include \
-  -lrt
+mex -O rosbag_wrapper.cpp parser.cpp  \
+  ../../../install/lib/librosbag.a \
+  ../../../install/lib/libroscpp.a \
+  ../../../install/lib/librostime.a \
+  ../../../install/lib/libroscpp_serialization.a \
+  ../../../install/lib/libboost_regex.a \
+  ../../../install/lib/libboost_system.a \
+  ../../../install/lib/libbz2.a \
+  -I../../../install/include \
+  -I/usr/local/include
