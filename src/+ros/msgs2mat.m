0a2de6722a4c7ab359050f36f7d17d5bc2e49019
Ben Charrow <bcharrow@seas.upenn.edu>
2014-03-31 17:33:19 -0400

    Add default accessor for msgs2mat

--- src/+ros/msgs2mat.m
+++ src/+ros/msgs2mat.m
@@ -4,12 +4,14 @@ function [mat] = msgs2mat(msgs, accessor, converter)
 % of fields in each message and N is the length of msgs.  accessor is a
 % function which takes a message and returns a vector or a struct whose entries
 % are numeric.  If a struct is returned, it is converted to a vector by stacking
-% its fields in order.
+% its fields in order.  By default, accessor is the identity function.
 %
 % MAT = MSG2MAT(msgs, accessor, converter)  Apply the function converter to each
 % struct from accessor.  converter should take the output of accessor and
 % return a vector.
-
+if nargin < 2 || isempty(accessor)
+    accessor = @(x) x;
+end
 if nargin < 3
     converter = @default_converter;
 end
